12ca74eca248ade5f1a56b1820506aee8fa7da21
scign <scignspam.github@mailnull.com>
2020-03-07 14:37:58 -0500

    Initial flask framework

---
+++ app/config.py
@@ -0,0 +1,22 @@
+class Config(object):
+    TESTING = False
+    DEVELOPMENT = False
+    DEBUG = False
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
